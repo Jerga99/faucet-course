136ee4e88551f36c6baa34f0deea30785b7cd0b8
Filip Jerga <jerga99@gmail.com>
2021-08-15 19:12:17 +0200

    Installing Ganache

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
   networks: {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     }
   },
