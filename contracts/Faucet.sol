5f35747ca4edfceddcc841b4b1ef2754001ce6a9
Filip Jerga <jerga99@gmail.com>
2021-08-15 19:36:57 +0200

    First smart contract

---
+++ contracts/Faucet.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+  // storage variables
+  uint public funds = 1000; // positive values only
+  int public counter = -10;
+}
