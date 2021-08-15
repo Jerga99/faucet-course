67b6a77965fb6dbd6a97a48b7a9cbdcedc2fa77d
Filip Jerga <jerga99@gmail.com>
2021-08-15 18:54:43 +0200

    Truffle init

---
+++ truffle-config.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
+};
