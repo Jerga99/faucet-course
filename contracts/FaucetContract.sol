7dbc9f72541208cc0e5f337c4f51e9ba3dfda2d9
Filip Jerga <jerga99@gmail.com>
2021-08-17 10:16:16 +0200

    Send transaction

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,5 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
+
+  // this is a special function
+  // it's called when you make a tx that doesn't specify
+  // function name to call
+
+  // External function are part of the contract interface
+  // which means they can be called via contracts and other txs
+
   receive() external payable {}
 }
