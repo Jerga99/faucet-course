710449060a916a1c8e7bafa18aaad321d2f2e181
Filip Jerga <jerga99@gmail.com>
2021-08-17 10:04:49 +0200

    Receive function

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,8 +2,5 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-  // storage variables
-  uint public funds = 1000; // positive values only
-  int public counter = -10;
-  uint32 public test = 4294967295;
+  receive() external payable {}
 }
--- truffle-config.js
+++ truffle-config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.4",    // Fetch exact version from solc-bin (default: truffle's version)
     }
   }
 };
