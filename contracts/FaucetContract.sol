39df0ff22fcd40a27f07d9fcfaf554876607548d
Filip Jerga <jerga99@gmail.com>
2021-08-21 16:35:10 +0200

    lutFunders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -5,6 +5,7 @@ contract Faucet {
 
   uint public numOfFunders;
   mapping(address => bool) private funders;
+  mapping(uint => address) private lutFunders;
 
   receive() external payable {}
 
@@ -12,11 +13,25 @@ contract Faucet {
     address funder = msg.sender;
 
     if (!funders[funder]) {
-      numOfFunders++;
+      uint index = numOfFunders++;
       funders[funder] = true;
+      lutFunders[index] = funder;
     }
   }
 
+  function getAllFunders() external view returns (address[] memory) {
+    address[] memory _funders = new address[](numOfFunders);
+
+    for (uint i = 0; i < numOfFunders; i++) {
+      _funders[i] = lutFunders[i];
+    }
+
+    return _funders;
+  }
+
+  function getFunderAtIndex(uint8 index) external view returns(address) {
+    return lutFunders[index];
+  }
 }
 
 
