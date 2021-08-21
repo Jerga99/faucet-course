d761d76c7d497c3faa8cd2768e4836692823302b
Filip Jerga <jerga99@gmail.com>
2021-08-21 16:26:44 +0200

    Prevent duplications

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,28 +4,19 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
 
   uint public numOfFunders;
-  mapping(uint => address) private funders;
+  mapping(address => bool) private funders;
 
   receive() external payable {}
 
   function addFunds() external payable {
-    uint index = numOfFunders++;
-    funders[index] = msg.sender;
-  }
+    address funder = msg.sender;
 
-  function getAllFunders() external view returns (address[] memory) {
-    address[] memory _funders = new address[](numOfFunders);
-
-    for (uint i = 0; i < numOfFunders; i++) {
-      _funders[i] = funders[i];
+    if (!funders[funder]) {
+      numOfFunders++;
+      funders[funder] = true;
     }
-
-    return _funders;
   }
 
-  function getFunderAtIndex(uint8 index) external view returns(address) {
-    return funders[index];
-  }
 }
 
 
