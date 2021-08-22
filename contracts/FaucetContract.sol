ab20cb85d5e0e294fc82951c43a3d605a754857c
Filip Jerga <jerga99@gmail.com>
2021-08-22 15:13:19 +0200

    Only owner

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,8 +4,22 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
 
   uint public numOfFunders;
+  address public owner;
+
   mapping(address => bool) private funders;
   mapping(uint => address) private lutFunders;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner {
+    require(
+      msg.sender == owner,
+      "Only owner can call this function"
+    );
+    _;
+  }
 
   modifier limitWithdraw(uint withdrawAmount) {
     require(
@@ -25,6 +39,14 @@ contract Faucet {
       funders[funder] = true;
       lutFunders[index] = funder;
     }
+  }
+
+  function test1() external onlyOwner {
+    // some managing stuff that only admin should have access to
+  }
+
+  function test2() external onlyOwner {
+    // some managing stuff that only admin should have access to
   }
 
   function withdraw(uint withdrawAmount) external limitWithdraw(withdrawAmount) {
