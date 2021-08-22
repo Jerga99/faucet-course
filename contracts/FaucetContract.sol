c58c8b35aab38fba023800b83a98566c1f11a00f
Filip Jerga <jerga99@gmail.com>
2021-08-22 16:00:20 +0200

    Abstract contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 import "./Owned.sol";
+import "./Logger.sol";
 
-contract Faucet is Owned {
+contract Faucet is Owned, Logger {
   uint public numOfFunders;
 
   mapping(address => bool) private funders;
@@ -17,6 +18,10 @@ contract Faucet is Owned {
   }
 
   receive() external payable {}
+
+  function emitLog() public override pure returns(bytes32) {
+    return "Hello World";
+  }
 
   function addFunds() external payable {
     address funder = msg.sender;
---
+++ contracts/Logger.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+// It's a way for designer to say that
+// "any child of the abstract contract has to implmenet speicifed methods"
+
+abstract contract Logger {
+
+  uint public testNum;
+
+  constructor() {
+    testNum = 1000;
+  }
+
+  function emitLog() public pure virtual returns(bytes32);
+
+  function test3() external pure returns(uint) {
+    return 100;
+  }
+}
