f2f530a3ec9ff7380cdc23d7e639b27ee7840981
Filip Jerga <jerga99@gmail.com>
2021-08-24 10:31:34 +0200

    Add bulma

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
+import "./App.css";
+
+
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bulma/css/bulma.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
