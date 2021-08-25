defa833f99495f0956d93bb00e9acc6c4f7476a0
Filip Jerga <jerga99@gmail.com>
2021-08-25 10:11:00 +0200

    Connect on click

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
       const provider = await detectEthereumProvider()
 
       if (provider) {
-        provider.request({method: "eth_requestAccounts"})
         setWeb3Api({
           web3: new Web3(provider),
           provider
@@ -44,17 +43,29 @@ function App() {
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <span>
-            <strong>Account: </strong>
-          </span>
-          <h1>
-            { account ? account : "not connected" }
-          </h1>
-          <div className="balance-view is-size-2">
+          <div className="is-flex is-align-items-center">
+            <span>
+              <strong className="mr-2">Account: </strong>
+            </span>
+              { account ?
+                <div>{account}</div> :
+                <button
+                  className="button is-small"
+                  onClick={() =>
+                    web3Api.provider.request({method: "eth_requestAccounts"}
+                  )}
+                >
+                  Connect Wallet
+                </button>
+              }
+          </div>
+          <div className="balance-view is-size-2 my-4">
             Current Balance: <strong>10</strong> ETH
           </div>
-          <button className="btn mr-2">Donate</button>
-          <button className="btn">Withdraw</button>
+          <button
+            className="button is-link mr-2">Donate</button>
+          <button
+            className="button is-primary">Withdraw</button>
         </div>
       </div>
     </>
