a104ab6f70da8437755310958a8140de62f8dd83
Filip Jerga <jerga99@gmail.com>
2021-08-25 09:45:27 +0200

    request accounts

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         provider = window.ethereum;
 
         try {
-          await provider.enable();
+          await provider.request({method: "eth_requestAccounts"})
         } catch {
           console.error("User denied accounts access!")
         }
