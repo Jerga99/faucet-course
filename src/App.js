377cbe7affd6dd21e640ab120093e3fc1eca5907
Filip Jerga <jerga99@gmail.com>
2021-08-26 08:43:27 +0200

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
           isProviderLoaded: true
         })
       } else {
-        setWeb3Api({...web3Api, isProviderLoaded: true})
+        setWeb3Api(api => ({...api, isProviderLoaded: true}))
         console.error("Please, install Metamask.")
       }
     }
@@ -99,7 +99,10 @@ function App() {
                   <>
                     <div className="notification is-warning is-size-6 is-rounded">
                       Wallet is not detected!{` `}
-                      <a target="_blank" href="https://docs.metamask.io">
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://docs.metamask.io">
                         Install Metamask
                       </a>
                     </div>
